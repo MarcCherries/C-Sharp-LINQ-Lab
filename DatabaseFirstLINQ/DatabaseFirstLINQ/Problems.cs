256cbd005556fb9bbac874baab4ef6675b889706
Chris Creamer <chrisc1983@gmail.com>
2022-04-26 06:39:35 -0700

    added one line to problem 10 that I noticed had been omitted, where the roleType variable is used in the second Linq query

--- DatabaseFirstLINQ/DatabaseFirstLINQ/Problems.cs
+++ DatabaseFirstLINQ/DatabaseFirstLINQ/Problems.cs
@@ -159,202 +159,190 @@ namespace DatabaseFirstLINQ
 
         //}
 
-        //private void ProblemTen()
-        //{
-        //    //Write a LINQ query that retreives all of the products in the shopping cart of users who have the role of "Employee".
-        //    // Then print the user's email as well as the product's name, price, and quantity to the console.
-        //    {
-        //        //Write a LINQ query that retreives all of the products in the shopping cart of users who have the role of "Employee".
-        //        // Then print the user's email as well as the product's name, price, and quantity to the console.
+        private void ProblemTen()
+        {
+            //Write a LINQ query that retreives all of the products in the shopping cart of users who have the role of "Employee".
+            // Then print the user's email as well as the product's name, price, and quantity to the console.
+            {
+                //Write a LINQ query that retreives all of the products in the shopping cart of users who have the role of "Employee".
+                // Then print the user's email as well as the product's name, price, and quantity to the console.
 
-        //        var roleType = _context.Roles.Where(r => r.RoleName == "Employee").Include(r => r.UserRoles);
-        //        var products = _context.ShoppingCarts.Include(sc => sc.Product).Where(sc => sc.).Select(sc => sc.Product);
-
-
-        //        foreach (Product product in products)
-        //        {
-        //            Console.WriteLine(product.Name);
-        //        }
+                var roleType = _context.Roles.Where(r => r.RoleName == "Employee").Include(r => r.UserRoles);
+                var products = _context.ShoppingCarts.Include(sc => sc.Product).Where(sc => sc.User.UserRoles == roleType).Select(sc => sc.Product);
 
 
+                foreach (Product product in products)
+                {
+                    Console.WriteLine(product.Name);
+                }
 
 
+                //    // <><><><><><><><> CUD(Create, Update, Delete) Actions<><><><><><><><><>
+
+                //    // <><> C Actions(Create) <><>
+                //}
+                ////private void ProblemEleven()
+                //{
+                //    //Create a new User object and add that user to the Users table using LINQ.
+                //    User newUser = new User()
+                //    {
+                //        Email = "david@gmail.com",
+                //        Password = "DavidsPass123"
+                //    };
+                //    _context.Users.Add(newUser);
+                //    _context.SaveChanges();
+                //}
+
+                //private void ProblemTwelve()
+                //{
+                //    //    Create a new Product object and add that product to the Products table using LINQ.
+                //    Product product = new Product()
+                //    {
+                //        Name = "Tablet",
+                //        Description = "Apple Ipad 32 GB",
+                //        Price = 700
+                //    };
+                //    _context.Products.Add(product);
+                //    _context.SaveChanges();
+
+                //}
+
+                //private void ProblemThirteen()
+                //{
+                //    //Add the role of "Customer" to the user we just created in the UserRoles junction table using LINQ.
+                //    var roleId = _context.Roles.Where(r => r.RoleName == "Customer").Select(r => r.Id).SingleOrDefault();
+                //    var userId = _context.Users.Where(u => u.Email == "david@gmail.com").Select(u => u.Id).SingleOrDefault();
+                //    UserRole newUserRole = new UserRole()
+                //    {
+                //        UserId = userId,
+                //        RoleId = roleId
+                //    };
+                //    _context.UserRoles.Add(newUserRole);
+                //    _context.SaveChanges();
+                //}
+
+                //private void ProblemFourteen()
+                //{
+                //    //Add the product you create to the user we created in the ShoppingCart junction table using LINQ.
+                //    var productId = _context.Products.Where(p => p.Name == "Tablet").Select(p => p.Id).SingleOrDefault();
+                //    var userId = _context.Users.Where(u => u.Email == "oda@gmail.com").Select(u => u.Id).SingleOrDefault();
+                //    ShoppingCart newShoppingCart = new ShoppingCart()
+                //    {
+                //        UserId = userId,
+                //        ProductId = productId,
+                //        Quantity = 1
+                //    };
+                //    _context.ShoppingCarts.Add(newShoppingCart);
+                //    _context.SaveChanges();
+                //}
+
+                // <><> U Actions(Update) <><>
+
+                //private void ProblemFifteen()
+                //{
+                //    //Update the email of the user we created to "mike@gmail.com"
+                //    var user = _context.Users.Where(u => u.Email == "david@gmail.com").SingleOrDefault();
+                //    user.Email = "mike@gmail.com";
+                //    _context.Users.Update(user);
+                //    _context.SaveChanges();
+                //}
+
+                //private void ProblemSixteen()
+                //{
+                //    //Update the price of the product you created to something different using LINQ.
+
+                //    var productToUpdate = _context.Products.Where(p => p.Id == 8).SingleOrDefault();
+
+                //    productToUpdate.Price = 1050;
+
+                //    _context.Products.Update(productToUpdate);
+                //    _context.SaveChanges();
+                //}
+
+                //private void ProblemSeventeen()
+                //{
+                //    //Change the role of the user we created to "Employee"
+                //    // HINT: You need to delete the existing role relationship and then create a new UserRole object and add it to the UserRoles table
+                //    // See problem eighteen as an example of removing a role relationship
+                //    var userRole = _context.UserRoles.Where(ur => ur.User.Email == "mike@gmail.com").SingleOrDefault();
+                //    _context.UserRoles.Remove(userRole);
+                //    UserRole newUserRole = new UserRole()
+                //    {
+                //        UserId = _context.Users.Where(u => u.Email == "mike@gmail.com").Select(u => u.Id).SingleOrDefault(),
+                //        RoleId = _context.Roles.Where(r => r.RoleName == "Employee").Select(r => r.Id).SingleOrDefault()
+                //    };
+                //    _context.UserRoles.Add(newUserRole);
+                //    _context.SaveChanges();
+                //}
+
+                //<><> D Actions(Delete) <><>
+
+                //private void ProblemEighteen()
+                //{
+                //    Console.WriteLine("success!");
+                //    //Delete the role relationship from the user who has the email "oda@gmail.com" using LINQ.
+                //    var userRole = _context.UserRoles.Where(ur => ur.User.Email == "afton@gmail.com").SingleOrDefault();
+
+                //    _context.UserRoles.Remove(userRole);
+                //    _context.SaveChanges();
 
 
-
-
-
-
-
-        //    }
-
-
-        //    // <><><><><><><><> CUD(Create, Update, Delete) Actions<><><><><><><><><>
-
-        //    // <><> C Actions(Create) <><>
-        //}
-        ////private void ProblemEleven()
-        //{
-        //    //Create a new User object and add that user to the Users table using LINQ.
-        //    User newUser = new User()
-        //    {
-        //        Email = "david@gmail.com",
-        //        Password = "DavidsPass123"
-        //    };
-        //    _context.Users.Add(newUser);
-        //    _context.SaveChanges();
-        //}
-
-        //private void ProblemTwelve()
-        //{
-        //    //    Create a new Product object and add that product to the Products table using LINQ.
-        //    Product product = new Product()
-        //    {
-        //        Name = "Tablet",
-        //        Description = "Apple Ipad 32 GB",
-        //        Price = 700
-        //    };
-        //    _context.Products.Add(product);
-        //    _context.SaveChanges();
-
-        //}
-
-        //private void ProblemThirteen()
-        //{
-        //    //Add the role of "Customer" to the user we just created in the UserRoles junction table using LINQ.
-        //    var roleId = _context.Roles.Where(r => r.RoleName == "Customer").Select(r => r.Id).SingleOrDefault();
-        //    var userId = _context.Users.Where(u => u.Email == "david@gmail.com").Select(u => u.Id).SingleOrDefault();
-        //    UserRole newUserRole = new UserRole()
-        //    {
-        //        UserId = userId,
-        //        RoleId = roleId
-        //    };
-        //    _context.UserRoles.Add(newUserRole);
-        //    _context.SaveChanges();
-        //}
-
-        //private void ProblemFourteen()
-        //{
-        //    //Add the product you create to the user we created in the ShoppingCart junction table using LINQ.
-        //    var productId = _context.Products.Where(p => p.Name == "Tablet").Select(p => p.Id).SingleOrDefault();
-        //    var userId = _context.Users.Where(u => u.Email == "oda@gmail.com").Select(u => u.Id).SingleOrDefault();
-        //    ShoppingCart newShoppingCart = new ShoppingCart()
-        //    {
-        //        UserId = userId,
-        //        ProductId = productId,
-        //        Quantity = 1
-        //    };
-        //    _context.ShoppingCarts.Add(newShoppingCart);
-        //    _context.SaveChanges();
-        //}
-
-        // <><> U Actions(Update) <><>
-
-        //private void ProblemFifteen()
-        //{
-        //    //Update the email of the user we created to "mike@gmail.com"
-        //    var user = _context.Users.Where(u => u.Email == "david@gmail.com").SingleOrDefault();
-        //    user.Email = "mike@gmail.com";
-        //    _context.Users.Update(user);
-        //    _context.SaveChanges();
-        //}
-
-        //private void ProblemSixteen()
-        //{
-        //    //Update the price of the product you created to something different using LINQ.
-
-        //    var productToUpdate = _context.Products.Where(p => p.Id == 8).SingleOrDefault();
-
-        //    productToUpdate.Price = 1050;
-
-        //    _context.Products.Update(productToUpdate);
-        //    _context.SaveChanges();
-        //}
-
-        //private void ProblemSeventeen()
-        //{
-        //    //Change the role of the user we created to "Employee"
-        //    // HINT: You need to delete the existing role relationship and then create a new UserRole object and add it to the UserRoles table
-        //    // See problem eighteen as an example of removing a role relationship
-        //    var userRole = _context.UserRoles.Where(ur => ur.User.Email == "mike@gmail.com").SingleOrDefault();
-        //    _context.UserRoles.Remove(userRole);
-        //    UserRole newUserRole = new UserRole()
-        //    {
-        //        UserId = _context.Users.Where(u => u.Email == "mike@gmail.com").Select(u => u.Id).SingleOrDefault(),
-        //        RoleId = _context.Roles.Where(r => r.RoleName == "Employee").Select(r => r.Id).SingleOrDefault()
-        //    };
-        //    _context.UserRoles.Add(newUserRole);
-        //    _context.SaveChanges();
-        //}
-
-        //<><> D Actions(Delete) <><>
-
-        //private void ProblemEighteen()
-        //{
-        //    Console.WriteLine("success!");
-        //    //Delete the role relationship from the user who has the email "oda@gmail.com" using LINQ.
-        //    var userRole = _context.UserRoles.Where(ur => ur.User.Email == "afton@gmail.com").SingleOrDefault();
-
-        //    _context.UserRoles.Remove(userRole);
-        //    _context.SaveChanges();
-
-
-        //}
+                //}
 
 
 
 
-        //private void ProblemNineteen()
-        //{
-        //    //Delete all of the product relationships to the user with the email "oda@gmail.com" in the ShoppingCart table using LINQ.
-        //    //HINT: Loop
-        //   var shoppingCartProducts = _context.ShoppingCarts.Where(sc => sc.User.Email == "oda@gmail.com");
-        //    foreach (ShoppingCart userProductRelationship in shoppingCartProducts)
-        //    {
-        //        _context.ShoppingCarts.Remove(userProductRelationship);
-        //    }
-        //    _context.SaveChanges();
-        //}
+                //private void ProblemNineteen()
+                //{
+                //    //Delete all of the product relationships to the user with the email "oda@gmail.com" in the ShoppingCart table using LINQ.
+                //    //HINT: Loop
+                //   var shoppingCartProducts = _context.ShoppingCarts.Where(sc => sc.User.Email == "oda@gmail.com");
+                //    foreach (ShoppingCart userProductRelationship in shoppingCartProducts)
+                //    {
+                //        _context.ShoppingCarts.Remove(userProductRelationship);
+                //    }
+                //    _context.SaveChanges();
+                //}
 
-        //private void ProblemTwenty()
-        //{
-        //    //Delete the user with the email "oda@gmail.com" from the Users table using LINQ.
+                //private void ProblemTwenty()
+                //{
+                //    //Delete the user with the email "oda@gmail.com" from the Users table using LINQ.
 
-        //    var userToDelete = _context.Users.Where(u => u.Email == "oda@gmail.com").SingleOrDefault();
-        //    Console.WriteLine("========");
-        //    Console.WriteLine(userToDelete);
+                //    var userToDelete = _context.Users.Where(u => u.Email == "oda@gmail.com").SingleOrDefault();
+                //    Console.WriteLine("========");
+                //    Console.WriteLine(userToDelete);
 
-        //    _context.Users.Remove(userToDelete);
-        //    _context.SaveChanges();
+                //    _context.Users.Remove(userToDelete);
+                //    _context.SaveChanges();
 
-        //}
+                //}
 
-        //        // <><><><><><><><> BONUS PROBLEMS<><><><><><><><><>
+                //        // <><><><><><><><> BONUS PROBLEMS<><><><><><><><><>
 
-        //        private void BonusOne()
-        //{
-        //    //Prompt the user to enter in an email and password through the console.
-        //    // Take the email and password and check if the there is a person that matches that combination.
-        //    // Print "Signed In!" to the console if they exists and the values match otherwise print "Invalid Email or Password."
+                //        private void BonusOne()
+                //{
+                //    //Prompt the user to enter in an email and password through the console.
+                //    // Take the email and password and check if the there is a person that matches that combination.
+                //    // Print "Signed In!" to the console if they exists and the values match otherwise print "Invalid Email or Password."
 
-        //    Console.WriteLine("Enter email:");
-        //    string userEmail = Console.ReadLine();
+                //    Console.WriteLine("Enter email:");
+                //    string userEmail = Console.ReadLine();
 
-        //    Console.WriteLine("Enter password:");
-        //    string userPassword = Console.ReadLine();
+                //    Console.WriteLine("Enter password:");
+                //    string userPassword = Console.ReadLine();
 
-        //    if (_context.Users.Count((u) => u.Email == userEmail && u.Password == userPassword) == 0)
-        //    {
-        //        Console.WriteLine("Invalid Email or Password.");
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("Signed In!");
-        //    }
+                //    if (_context.Users.Count((u) => u.Email == userEmail && u.Password == userPassword) == 0)
+                //    {
+                //        Console.WriteLine("Invalid Email or Password.");
+                //    }
+                //    else
+                //    {
+                //        Console.WriteLine("Signed In!");
+                //    }
 
-        //}
+                //}
 
-        private void BonusTwo()
+                private void BonusTwo()
         {
             int grandTotal = 0;
 
